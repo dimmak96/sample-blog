f4b8ea355664889524911654a5e94797341263e5
Dmitriy <dimmak.260823@gmail.com>
2016-08-11 20:20:52 +0300

    Add devise model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+
+
 Rails.application.routes.draw do
+
+  root to: "home#index"
+  
   get 'home/index'
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
@@ -9,3 +14,5 @@ Rails.application.routes.draw do
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
